6dfac4dd5ea04e3305bafb68d897aa902c100b7a
Lasarussp <001lasarus@gmail.com>
2023-03-31 12:39:34 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/logo.png';
-import { HiOutlineMail } from 'react-icons/hi';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -11,6 +10,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
+        {/* eslint-disable-next-line */}
          <img src={Logo} alt='Logo Image' style={{width: '100px' }} />
       </div>
 
